728ecf8cec681ced038527afc221daebbcad970d
Sean Westley Matalog <westleymatalog9@gmail.com>
2026-09-23 21:45:39 +0800

    Add cashier dashboard

--- Login/Sales & Inventory Log In/CashierForm.Designer.cs
+++ Login/Sales & Inventory Log In/CashierForm.Designer.cs
@@ -29,26 +29,252 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblQuickAction = new System.Windows.Forms.Label();
+            this.btnNewSale = new System.Windows.Forms.Button();
+            this.btnViewSale = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblAverageOrderValue = new System.Windows.Forms.Label();
+            this.lblCustomerServed = new System.Windows.Forms.Label();
+            this.lbl711167 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lblTodaysSales = new System.Windows.Forms.Label();
+            this.lbl854000 = new System.Windows.Forms.Label();
+            this.lblTransaction = new System.Windows.Forms.Label();
+            this.lbl12 = new System.Windows.Forms.Label();
+            this.lblWelcomeCashier = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(275, 32);
+            this.label1.Size = new System.Drawing.Size(237, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cashier Dashboard";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.lblTodaysSales);
+            this.panel1.Controls.Add(this.lbl854000);
+            this.panel1.Location = new System.Drawing.Point(41, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 76);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel2.Controls.Add(this.lbl12);
+            this.panel2.Controls.Add(this.lblTransaction);
+            this.panel2.Location = new System.Drawing.Point(223, 85);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(158, 78);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel3.Controls.Add(this.lbl10);
+            this.panel3.Controls.Add(this.lblCustomerServed);
+            this.panel3.Location = new System.Drawing.Point(412, 85);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(158, 78);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel4.Controls.Add(this.lbl711167);
+            this.panel4.Controls.Add(this.lblAverageOrderValue);
+            this.panel4.Location = new System.Drawing.Point(605, 81);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(159, 80);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel5.Controls.Add(this.btnViewSale);
+            this.panel5.Controls.Add(this.btnNewSale);
+            this.panel5.Controls.Add(this.lblQuickAction);
+            this.panel5.Location = new System.Drawing.Point(471, 228);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(267, 239);
+            this.panel5.TabIndex = 6;
+            // 
+            // lblQuickAction
+            // 
+            this.lblQuickAction.AutoSize = true;
+            this.lblQuickAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuickAction.Location = new System.Drawing.Point(14, 11);
+            this.lblQuickAction.Name = "lblQuickAction";
+            this.lblQuickAction.Size = new System.Drawing.Size(116, 20);
+            this.lblQuickAction.TabIndex = 0;
+            this.lblQuickAction.Text = "Quick Action";
+            // 
+            // btnNewSale
+            // 
+            this.btnNewSale.BackColor = System.Drawing.Color.LightGray;
+            this.btnNewSale.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewSale.Location = new System.Drawing.Point(64, 76);
+            this.btnNewSale.Name = "btnNewSale";
+            this.btnNewSale.Size = new System.Drawing.Size(119, 48);
+            this.btnNewSale.TabIndex = 1;
+            this.btnNewSale.Text = "New Sale";
+            this.btnNewSale.UseVisualStyleBackColor = false;
+            // 
+            // btnViewSale
+            // 
+            this.btnViewSale.BackColor = System.Drawing.Color.LightGray;
+            this.btnViewSale.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewSale.Location = new System.Drawing.Point(64, 148);
+            this.btnViewSale.Name = "btnViewSale";
+            this.btnViewSale.Size = new System.Drawing.Size(119, 45);
+            this.btnViewSale.TabIndex = 2;
+            this.btnViewSale.Text = "View Sale";
+            this.btnViewSale.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(385, 239);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // lblAverageOrderValue
+            // 
+            this.lblAverageOrderValue.AutoSize = true;
+            this.lblAverageOrderValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverageOrderValue.Location = new System.Drawing.Point(3, 6);
+            this.lblAverageOrderValue.Name = "lblAverageOrderValue";
+            this.lblAverageOrderValue.Size = new System.Drawing.Size(153, 16);
+            this.lblAverageOrderValue.TabIndex = 0;
+            this.lblAverageOrderValue.Text = "Average Order Value";
+            // 
+            // lblCustomerServed
+            // 
+            this.lblCustomerServed.AutoSize = true;
+            this.lblCustomerServed.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerServed.Location = new System.Drawing.Point(14, 2);
+            this.lblCustomerServed.Name = "lblCustomerServed";
+            this.lblCustomerServed.Size = new System.Drawing.Size(126, 16);
+            this.lblCustomerServed.TabIndex = 0;
+            this.lblCustomerServed.Text = "Customer Served";
+            // 
+            // lbl711167
+            // 
+            this.lbl711167.AutoSize = true;
+            this.lbl711167.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl711167.Location = new System.Drawing.Point(37, 41);
+            this.lbl711167.Name = "lbl711167";
+            this.lbl711167.Size = new System.Drawing.Size(61, 16);
+            this.lbl711167.TabIndex = 1;
+            this.lbl711167.Text = "₱711.67";
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl10.Location = new System.Drawing.Point(65, 39);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(23, 16);
+            this.lbl10.TabIndex = 1;
+            this.lbl10.Text = "10";
+            // 
+            // lblTodaysSales
+            // 
+            this.lblTodaysSales.AutoSize = true;
+            this.lblTodaysSales.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTodaysSales.Location = new System.Drawing.Point(3, 2);
+            this.lblTodaysSales.Name = "lblTodaysSales";
+            this.lblTodaysSales.Size = new System.Drawing.Size(108, 16);
+            this.lblTodaysSales.TabIndex = 8;
+            this.lblTodaysSales.Text = "Today\'s Sales";
+            // 
+            // lbl854000
+            // 
+            this.lbl854000.AutoSize = true;
+            this.lbl854000.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl854000.Location = new System.Drawing.Point(34, 39);
+            this.lbl854000.Name = "lbl854000";
+            this.lbl854000.Size = new System.Drawing.Size(73, 16);
+            this.lbl854000.TabIndex = 9;
+            this.lbl854000.Text = "₱8,540.00";
+            // 
+            // lblTransaction
+            // 
+            this.lblTransaction.AutoSize = true;
+            this.lblTransaction.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTransaction.Location = new System.Drawing.Point(3, 2);
+            this.lblTransaction.Name = "lblTransaction";
+            this.lblTransaction.Size = new System.Drawing.Size(101, 16);
+            this.lblTransaction.TabIndex = 10;
+            this.lblTransaction.Text = "Transaction\'s";
+            // 
+            // lbl12
+            // 
+            this.lbl12.AutoSize = true;
+            this.lbl12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl12.Location = new System.Drawing.Point(50, 39);
+            this.lbl12.Name = "lbl12";
+            this.lbl12.Size = new System.Drawing.Size(23, 16);
+            this.lbl12.TabIndex = 11;
+            this.lbl12.Text = "12";
+            // 
+            // lblWelcomeCashier
+            // 
+            this.lblWelcomeCashier.AutoSize = true;
+            this.lblWelcomeCashier.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcomeCashier.Location = new System.Drawing.Point(594, 29);
+            this.lblWelcomeCashier.Name = "lblWelcomeCashier";
+            this.lblWelcomeCashier.Size = new System.Drawing.Size(133, 16);
+            this.lblWelcomeCashier.TabIndex = 8;
+            this.lblWelcomeCashier.Text = "Welcome, Cashier";
+            // 
             // CashierForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(816, 535);
+            this.Controls.Add(this.lblWelcomeCashier);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "CashierForm";
             this.Text = "CashierForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +283,23 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnViewSale;
+        private System.Windows.Forms.Button btnNewSale;
+        private System.Windows.Forms.Label lblQuickAction;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblTodaysSales;
+        private System.Windows.Forms.Label lbl854000;
+        private System.Windows.Forms.Label lbl12;
+        private System.Windows.Forms.Label lblTransaction;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lblCustomerServed;
+        private System.Windows.Forms.Label lbl711167;
+        private System.Windows.Forms.Label lblAverageOrderValue;
+        private System.Windows.Forms.Label lblWelcomeCashier;
     }
 }
